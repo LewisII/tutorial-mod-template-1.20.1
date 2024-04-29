b3b7c5b8ab00bd226cb990e2eb2964ee94784971
Lewis <lewamos2@gmail.com>
2024-04-29 18:37:03 -0500

    inital commit, datagen is broken

---
+++ src/main/java/amos/games/tutorialmod/TutorialMod.java
@@ -0,0 +1,25 @@
+package amos.games.tutorialmod;
+
+import amos.games.tutorialmod.block.ModBlocks;
+import amos.games.tutorialmod.item.ModItemGroups;
+import amos.games.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
+	}
+}
---
+++ src/main/java/amos/games/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package amos.games.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
